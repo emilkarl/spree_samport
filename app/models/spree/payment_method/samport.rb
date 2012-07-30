7e49864a732d64c4c182ecb802b443f3ddfbe8f1
Emil Karlsson <emil.karl@gmail.com>
2012-07-30 23:33:49 +0200

    Updated attr_accessible, bumped version 1.0.10

--- app/models/spree/payment_method/samport.rb
+++ app/models/spree/payment_method/samport.rb
@@ -5,6 +5,8 @@ class Spree::PaymentMethod::Samport < Spree::PaymentMethod
   preference :iso_language_code, :string, :default => 'SE' 
   preference :iso_currency, :integer, :default => 752
   
+  attr_accessible :terminal_id, :preferred_terminal_id, :transaction_type, :preferred_transaction_type, :direct_capture, :preferred_direct_capture, :iso_language_code, :preferred_iso_language_code, :iso_currency, :preferred_iso_currency
+  
   def source_required?
     true
   end
